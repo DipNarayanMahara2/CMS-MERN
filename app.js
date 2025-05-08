a2551149d794b48eecc0f1e747067e214bc34e19
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-05-08 10:54:51 +0545

    Learning about MERN: Todays learning install expressjs, nodemon, creating Get APIs

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    // res.send('Hello World! Learning MERN Stack!');
+    res.json({
+        message: "Hello World! This is Home Page and I am Learning MERN Stack!",
+        status: 200,
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.json({
+        message: "Hello World! This is About Page and I am Learning MERN Stack!",
+        status: 200,
+    })
+})
+
+app.get("/contact", (req, res) => {
+    res.json({
+        message: "Hello World! This is Contact Page and I am Learning MERN Stack!",
+        status: 200,
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
