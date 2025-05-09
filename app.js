b2faf0e8cafb734cebde0a0d198aa4a8169825c7
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-05-09 18:29:07 +0545

    modified DB connect Function

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ const { connectDB } = require('./database/database');
 const app = express();
 
 // connecting to database
-// First way to connect to database
-// const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb+srv://dipnarayanmahara28:Dipnarayanmahara@project1.b4mntu1.mongodb.net/?retryWrites=true&w=majority&appName=Project1")
-//     .then(() => {
-//         console.log("Database Connected Successfully");
-//     })
-
-// second way to connect to database
 connectDB();
 
 // GET API 
