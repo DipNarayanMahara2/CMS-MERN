8a502dcacc0fd31470c5a703716cc0ef1331b6ba
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-05-10 12:04:15 +0545

    Connecting DB and creating first table in DB for Create Blog

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // importing express
 const express = require('express');
 const { connectDB } = require('./database/database');
+const Blog = require('./Model/BlogModal');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // connecting to database
 
@@ -17,25 +21,31 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get("/about", (req, res) => {
+// Create Blog API
+
+app.post("/createBlog", async (req, res) => {
+
+    // fist way to get data from body
+    // const title = req.body.title;
+    // const subTitle = req.body.subTitle;
+    // const description = req.body.description;
+
+    // second way to get data from body
+    const { title, subTitle, description } = req.body;
+
+    // inserting data into databse
+    await Blog.create({
+        title: title,
+        subTitle: subTitle,
+        description: description,
+    })
     res.json({
-        message: "Hello World! This is About Page and I am Learning MERN Stack!",
-        status: 200,
+        status: 201,
+        message: "Blog Created Successfully!"
     })
 })
 
-app.get("/contact", (req, res) => {
-    res.json({
-        message: "Hello World! This is Contact Page and I am Learning MERN Stack!",
-        status: 200,
-    })
-})
-app.get("/services", (req, res) => {
-    res.json({
-        message: "Hello World! This is Services Page and I am Learning MERN Stack!",
-        status: 200,
-    })
-})
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
